763ee72f34af3c6b1cf31e6a4ac49499691e597d
Dusan <mac@n188-p44.eduroam.kth.se>
2018-09-27 20:17:50 +0200

    Started querying database

--- test.py
+++ test.py
@@ -47,7 +47,45 @@ def get_data(cur, M):
     cur.execute("SELECT * FROM exhibitors_exhibitor WHERE fair_id = 3")
     rows = cur.fetchall()
 
-    for row in rows:
+    number_checkboxes = 0
+    for answer_list in M:
+        number_checkboxes += len(answer_list)
+
+    number_companies = len(rows)
+
+    final_matrix = np.zeros((number_companies, number_checkboxes), dtype=int)
+
+    for i,row in enumerate(rows):
+        print()
+        print(row[0])
+        cur.execute("SELECT DISTINCT exhibitor_id, cataloguebenefit_id  \
+                     FROM exhibitors_exhibitor_catalogue_benefits, exhibitors_exhibitor \
+                     WHERE exhibitors_exhibitor_catalogue_benefits.exhibitor_id = "  + str(row[0]) +  " \
+                     ORDER BY exhibitor_id, cataloguebenefit_id")
+        benefit_checkboxes = cur.fetchall()
+        benefit_checkbox_indexes = np.zeros (len(M[0]), dtype=int)
+        for checkbox in benefit_checkboxes:
+            print(checkbox[1])
+            benefit_checkbox_indexes[checkbox[1] - 1] = 1
+        print(benefit_checkbox_indexes)
+        final_matrix[i][np.where(benefit_checkbox_indexes > 0)[0]] = 1
+        print(final_matrix[i])
+
+        # cur.execute("SELECT DISTINCT exhibitor_id, catalogueemployment_id  \
+        #              FROM exhibitors_exhibitor_catalogue_employments, exhibitors_exhibitor \
+        #              WHERE exhibitors_exhibitor_catalogue_employments.exhibitor_id = "  + str(row[0]) +  " \
+        #              ORDER BY exhibitor_id, catalogueemployment_id")
+        # employment_checkboxes = cur.fetchall()
+        # employment_checkbox_indexes = np.zeros (len(M[1]), dtype=int)
+        # for checkbox in employment_checkboxes:
+        #     employment_checkbox_indexes[checkbox[1] - 1] = 1
+        # final_matrix[i] = np.concatenate((final_matrix[i],employment_checkbox_indexes))
+        # print(employment_checkboxes)
+        # print(final_matrix[i])
+
+        
+
+        
         print("Company: " + str(row[0]))
 
 
