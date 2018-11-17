cbed842b257ed5983af5a7fb3db4226bab0d5808
Dusan <mac@n128-p77.eduroam.kth.se>
2018-11-17 11:49:33 +0100

    Adding matching.py file from branch new-version

--- prod/matching.py
+++ prod/matching.py
@@ -2,7 +2,6 @@
 import json
 import numpy as np
 import psycopg2
-from math import*
 
 # File where data is fetched from the database
 import data_fetch
@@ -14,59 +13,43 @@ def enable_connection():
         print("I am unable to connect to the database")
     return conn.cursor()
 
-def euclidean(student_answers, company):
-    return np.sqrt(sum(pow(a-b,2) for a, b in zip(student_answers, company)))
-
-def manhattan_distance(student_answers,company):
-    return sum(abs(a-b) for a,b in zip(student_answers,company))
-    
-def square_rooted(x, rounded):
-    return round(sqrt(sum([a*a for a in x])),rounded)
- 
-def cosine_similarity(student_answers,company):
-    rounded= 15
-    numerator = sum(a*b for a,b in zip(student_answers,company))
-    denominator = square_rooted(student_answers,rounded)*square_rooted(company, rounded)
-    return round(numerator/float(denominator),rounded)
 
 def similarity_func(student_answers, company_answers, company_data, number_similar_companies):
-    
+    print student_answers
+    student_yes_answers = []
+
+    # Get all the answers the student marked
+    for i in range(len(student_answers)):
+        if student_answers[i] == 1:
+            student_yes_answers.append(i)
+
+    # Only keep the answers that the student chose
+    new_company_answers = company_answers[:, student_yes_answers]
+
     distances = {}
-    distance_cosine=np.zeros(len(company_answers), dtype=float)
-    distance_euclidean=np.zeros(len(company_answers), dtype=float)
-
-    # Eps is needed in case the max value is == 0 
-    #   which implies invalid division and that 
-    #   may happen in case of a exact match between
-    #   company and the student
-    eps = 0.0000000001
-    for i,company in enumerate(company_answers):
-        distance_cosine[i] = cosine_similarity(student_answers + eps, company + eps)
-        distance_euclidean[i] = euclidean(student_answers + eps, company + eps)
-    
-    # Normalize the vectors in order to be able
-    #   to sum them and get the final distance measure
-    normalized_distance_cosine = distance_cosine/ max(distance_cosine)
-    normalized_distance_euclidean = distance_euclidean/ max(distance_euclidean)
-    for i,company in enumerate(company_answers):
-        distances[i] = normalized_distance_cosine[i] + normalized_distance_euclidean[i]
-
+    for i,company in enumerate(new_company_answers):
+        # If a company has not answered, set the distance to max
+        if np.all(company==0):
+            distances[i] = 100000
+        else:
+            distances[i] = np.sqrt(sum(pow(a-b,2) for a, b in zip(student_yes_answers, company)))
     distances_sorted = []
     for key, value in sorted(distances.iteritems(), key=lambda kv: kv[1]):
         distances_sorted.append((key, value))
-
+    k = 0
     most_similar_companies = {}
     for i in range(number_similar_companies):
-        company_id = distances_sorted[i]
+        company = distances_sorted[i]
+        company_id = company[0]
+        exhibitor_id = company_data[company_id][0]
+        data_fetch.test_data_fetch(exhibitor_id)
         most_similar_companies[i] = {
-            "exhibitor_id": company_data[company_id[0]][0],
-            "distance": company_id[1]
+            "exhibitor_id": exhibitor_id,
+            "distance": company[1]
         }
-    
     with open('data.json', 'w') as outfile:
         json.dump(most_similar_companies, outfile)
-    
-    #print most_similar_companies
+    print most_similar_companies
     return most_similar_companies
 
 def matching(file_path):
