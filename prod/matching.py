4a721565dae190bd87466edf977fd418ab50626f
Svante Sörberg <svantesorberg@outlook.com>
2019-09-22 17:54:15 +0000

    Replaced 'distance' with normalized similarity measure

--- prod/matching.py
+++ prod/matching.py
@@ -15,39 +15,97 @@ def enable_connection():
     return conn.cursor()
 
 
-def similarity_func(student_answers, company_answers, company_data, number_similar_companies, doc_id):
+def similarity_func(student_data, company_data, number_similar_companies, doc_id):
     student_yes_indexes = []
 
     # Get all the answers the student marked
+    student_answers = student_data["answers"]
     for i in range(len(student_answers)):
         if student_answers[i] == 1:
             student_yes_indexes.append(i)
     new_student_answers = np.ones(len(student_yes_indexes))
 
     # Only keep the answer options that the student answered
-    new_company_answers = company_answers[:, student_yes_indexes]
-    import sys
-    distances = {}
+    new_company_answers = company_data["answers"][:, student_yes_indexes]
+    
+    # The maximum similarity obtainable is the number of 
+    # questions answered + the number of cities entered.
+    # The minimum is -1*the number of questions answered
+    # minus 1 if the student has picked any cities
+    max_points = len(student_yes_indexes)
+    min_points = -1*max_points
+    # Compare student and companies based on their selected answers
+    #distances = {}
+    similarities = {}
     for i,company in enumerate(new_company_answers):
+        # OLD COMPARISON
         # If a company has not answered, set the distance to a high value
-        #sys.stderr.write('company: ' + str(company))
-        if np.all(company == 0):
-            distances[i] = 100000
-        else:
-            distances[i] = np.sqrt(sum(pow(a-b,2) for a, b in zip(new_student_answers, company)))
+        # if np.all(company == 0):
+        #     distances[i] = 100000
+        # else:
+        #     distances[i] = np.sqrt(sum(pow(a-b,2) for a, b in zip(new_student_answers, company)))
 
-    distances_sorted = []
-    for key, value in sorted(distances.items(), key=lambda kv: kv[1]):
-        distances_sorted.append((key, value))
+        # NEW COMPARISON: Don't measure distance, measure similarity
+        # If the student answered yes, the company gets +1 similarity
+        # if they also picked yes, and -1 similarity if they didn't
+        # We may then normalize it, taking into account the worst
+        # possible similarity, and the best possible similarity.
+        # The best possible is if the company has answered yes
+        # for everything the student checked.
+        similarities[i] = sum((1 if a == b else -1 for a, b in zip(new_student_answers, company)))
+        import sys
+        sys.stderr.write("similarity: " + str(similarities[i]))
+        # The similarity is AT MOST 
+
+
+    # Compare student and companies based on the cities they entered
+    # Split the student choices at comma and remove whitespace
+    student_cities = [x.strip() for x in student_data["cities"].split(',')]
+    student_cities = [x for x in student_cities if x] # We do not accept empty strings    
+
+    sys.stderr.write("\n" + " cities: " + str(student_cities) + "\n")
+    if len(student_cities) > 0: # The student actually wrote something
+        max_points += len(student_cities)
+        min_points -= 1
+        for i, company_cities in enumerate(company_data["cities"]):
+            # TODO Fancy logic here
+            # If the company has not answered, they cannot have a match
+            if company_cities is None or not company_cities: # None or empty string
+                similarities[i] -= 1
+            else:
+                # Get the number of citites in common
+                number_of_matching_cities = sum([1 if city in company_cities else 0 for city in student_cities])
+                similarities[i] += number_of_matching_cities
+    
+    sys.stderr.write("Max points obtainable: " + str(max_points))
+    #distances_sorted = []
+    #for key, value in sorted(distances.items(), key=lambda kv: kv[1]):
+    similarities_sorted = []
+    for key, value in sorted(similarities.items(), key=lambda kv: kv[1], reverse=True):
+        #distances_sorted.append((key, value))
+        similarities_sorted.append((key, value))
     most_similar_companies = {}
+    # Now get the companies with the highest similarities
+    # Here we normalize the scores to values between 0 and 1.
+    # Currently, the values are between -1 - max_points
+    # and max_points.
+    # The normalization formula is thus:
+    # (x - (-1 - max_points)) / (2*max_points + 1)
     for i in range(number_similar_companies):
-        company = distances_sorted[i]
-        company_id = company[0]
-        exhibitor_id = company_data[company_id][0]
+        #company = distances_sorted[i]
+        #company_id = company[0]
+        #exhibitor_id = company_data[company_id][0]
         #data_fetch.test_data_fetch(exhibitor_id)
+        company = similarities_sorted[i]
+        company_index = company[0]
+        exhibitor_id = company_data["info"][company_index][0]
+
+        normalized_similarity = (company[1] - min_points) / (max_points - min_points) 
+
         most_similar_companies[i] = {
             "exhibitor_id": exhibitor_id,
-            "distance": company[1]
+            #"distance": company[1]
+            "similarity": normalized_similarity
         }
     with open("/tmp/" + doc_id + "_output.json", "w") as outfile:
         json.dump(most_similar_companies, outfile)
@@ -58,15 +116,16 @@ def matching(doc_id, file_path):
     with open(file_path, 'r') as infile:
         student_data_from_file = json.load(infile)
     student_data = format_student_data(cur, student_data_from_file)
-    company_answers = data_fetch.get_company_data(cur)
-    company_data = data_fetch.get_names_and_ids(cur)
-    most_similar_companies = similarity_func(student_data, company_answers, company_data, 5, doc_id)
+    company_data = data_fetch.get_company_data(cur)
+    #company_answers, company_cities = data_fetch.get_company_data(cur)
+    #company_data = data_fetch.get_names_and_ids(cur)
+    #most_similar_companies = similarity_func(student_data, company_answers, company_data, 5, doc_id)
+    most_similar_companies = similarity_func(student_data, company_data, 5, doc_id)
     return most_similar_companies
 
 
 def format_student_data(cur, data):
-    student_data = []
-
+    answers = []
     # Get the number of answers for each question
     number_of_answers = data_fetch.get_number_of_answers(cur)
 
@@ -79,27 +138,30 @@ def format_student_data(cur, data):
     competence_answer_indexes = np.zeros(number_of_answers[0], dtype=int)
     for answer in data.get("competences"):
         competence_answer_indexes[answer - 1] = 1
-    student_data = np.append(student_data, competence_answer_indexes)
+    answers = np.append(answers, competence_answer_indexes)
 
     employment_answer_indexes = np.zeros(number_of_answers[1], dtype=int)
     for answer in data.get("employments"):
         employment_answer_indexes[answer - 1] = 1
-    student_data = np.append(student_data,employment_answer_indexes)
+    answers = np.append(answers,employment_answer_indexes)
 
     industry_answer_indexes = np.zeros(number_of_answers[2], dtype=int)
     for answer in data.get("industries"):
         industry_answer_indexes[answer - 1] = 1
-    student_data = np.append(student_data, industry_answer_indexes)
+    answers = np.append(answers, industry_answer_indexes)
 
     value_answer_indexes = np.zeros(number_of_answers[3], dtype=int)
     for answer in data.get("values"):
         value_answer_indexes[answer - 1] = 1
-    student_data = np.append(student_data, value_answer_indexes)
+    answers = np.append(answers, value_answer_indexes)
 
     location_answer_indexes = np.zeros(number_of_answers[4], dtype=int)
     for answer in data.get("locations"):
         location_answer_indexes[answer - 1] = 1
-    student_data = np.append(student_data, location_answer_indexes)
+    answers = np.append(answers, location_answer_indexes)
+
+    student_data = {"answers": answers,
+                    "cities": data.get("cities")}
 
     return student_data
 
